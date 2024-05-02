28de6499bbbe18ccb99191acc5a4357de5ff6619
victor <162163319+v1c7o0r@users.noreply.github.com>
2024-05-02 08:09:01 +0300

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import { name, city } from "../data/data.js";
 function Home() {
   // update the JSX being returned!
   
-  return <div id="home">
+  return (
+    <div id="home">
    <h1 style={{ color: "firebrick" }}>
         {name} is a Web Developer from {city}
       </h1>
